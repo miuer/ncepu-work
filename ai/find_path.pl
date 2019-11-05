aa595fd572c53fc764bf03b0b08f7c1eb5bb5220
miuer <sy1440431095@gmail.com>
2019-11-05 09:22:44 +0800

    Update find_path.pl

--- ai/find_path.pl
+++ ai/find_path.pl
@@ -5,20 +5,20 @@
  */
 
 % You can customize the directed graph.
-road(a,b).
-road(a,e).
-road(b,c).
-road(b,d).
-road(c,g).
-road(d,c).
-road(e,d).
-road(e,f).
-road(f,g).
+road(a, b).
+road(a, e).
+road(b, c).
+road(b, d).
+road(c, g).
+road(d, c).
+road(e, d).
+road(e, f).
+road(f, g).
 
 % One-dimensional space solution.
-path(Y, Z) :- road(Y, Z), write(Y), write(" --> "),write(Z).
+path(Y, Z) :- road(Y, Z), write(Y), write(" --> "), write(Z).
 
 % Dimension upgrades
 path(X, Z) :- road(Y, Z), path(X, Y), write(" --> "), write(Z).
 
-goal(X,Z) :- path(X,Z).
+goal(X, Z) :- path(X, Z).
